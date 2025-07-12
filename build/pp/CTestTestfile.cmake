a37f5a78b29478c9d5d461c4b646535e39679ae0
Uday <163008205+venator158@users.noreply.github.com>
2025-07-12 15:20:55 +0530

    Delete build directory

--- build/pp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/venator/pil/dirac/pibt_ws/src/pp
-# Build directory: /home/venator/pil/dirac/pibt_ws/build/pp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
